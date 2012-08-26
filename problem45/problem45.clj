73eb558dcedf9c034ddc1df2cd9ede68206a0784
Han <hkfortytwo@gmail.com>
2012-08-26 02:12:06 -0700

    figured out how to pass functions into functions, refactored accordingly

--- problem45/problem45.clj
+++ problem45/problem45.clj
@@ -7,40 +7,33 @@
 (defn hexagonal [n]
   (* n (- (* n 2) 1)))
 
-(defn loop-through-p [i target]
-  (loop [pi i p (pentagonal pi)]
-    (if (= p target)
-      true
-      (if (> p target)
-          false
-          (recur (+ pi 1) (pentagonal (+ pi 1)))))))
-(defn loop-through-t [i target]
-  (loop [pi i p (triangle pi)]
-    (if (= p target)
-      true
-  (if (> p target)
-      false
-      (recur (+ pi 1) (triangle (+ pi 1)))))))
+(defn loop-through [tph]
+  (let [thisfunction tph]
+    (fn [i target]
+      (loop [pi i p (thisfunction pi)]
+      (if (= p target)
+            true
+        (if (> p target)
+            false
+            (recur (+ pi 1) (thisfunction (+ pi 1)))))))))
 
-(defn pindexgrabber [i target]
-  (loop [pi i p (pentagonal pi)]
-    (if (= p target)
-      pi
-      (if (> p target)
-          (- pi 1)
-          (recur (+ pi 1) (pentagonal (+ pi 1)))))))
-(defn tindexgrabber [i target]
-  (loop [pi i p (triangle pi)]
-    (if (= p target)
-      pi
-      (if (> p target)
-          (- pi 1)
-          (recur (+ pi 1) (triangle (+ pi 1)))))))
-
+(defn indexgrabber [funct]
+  (let [thisfunct funct]
+      (fn [i target]
+    (loop [pi i p (thisfunct pi)]
+        (if (= p target)
+          pi
+          (if (> p target)
+              (- pi 1)
+              (recur (+ pi 1) (thisfunct (+ pi 1)))))))))
+(def ltp (loop-through pentagonal))
+(def ltt (loop-through triangle))
+(def pindexgrabber (indexgrabber pentagonal))
+(def tindexgrabber (indexgrabber triangle))
 
 (loop [hindex 144 pindex 166 tindex 286 target (hexagonal hindex)]
-  (if-not (loop-through-p pindex target)
+  (if-not (ltp pindex target)
     (recur (+ hindex 1) (pindexgrabber pindex target) (pindexgrabber pindex target) (hexagonal (+ hindex 1)))
-    (if-not (loop-through-t tindex target)
+    (if-not (ltt tindex target)
     (recur (+ hindex 1) (pindexgrabber pindex target) (tindexgrabber tindex target) (hexagonal (+ hindex 1)))
       (println target))))
